4b892731f45827adb7ad0f495c7726dc8ac04b2b
Christian Rasmussen <christian.rasmussen@me.com>
2023-01-03 21:03:11 +0100

    Convert TestConfig to TestCase

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,45 @@ struct TestConfig {
     expected_exit_code: Option<i32>,
 }
 
+impl TestConfig {
+    fn to_test_case(self) -> Result<TestCase, TestConfigError> {
+        let mut assertions = vec![];
+        if let Some(stdout) = self.expected_stdout { assertions.push(TestAssertion::AssertStdout(stdout)) }
+        if let Some(stderr) = self.expected_stderr { assertions.push(TestAssertion::AssertStderr(stderr)) }
+        if let Some(exit_code) = self.expected_exit_code { assertions.push(TestAssertion::AssertExitCode(exit_code)) }
+
+        if assertions.len() == 0 {
+            return Err(TestConfigError::ExpectationRequired)
+        }
+
+        Ok(TestCase {
+            program: self.test_program,
+            arguments: self.test_arguments.unwrap_or(vec![]),
+            stdin: self.test_stdin,
+            assertions,
+        })
+    }
+}
+
+#[derive(Debug)]
+enum TestConfigError {
+    ExpectationRequired,
+}
+
+struct TestCase {
+    program: String,
+    arguments: Vec<String>,
+    stdin: Option<String>,
+    assertions: Vec<TestAssertion>,
+}
+
+enum TestAssertion {
+    AssertStdout(String),
+    AssertStderr(String),
+    AssertExitCode(i32),
+}
+
+
 fn main() {
     let args = Args::parse();
 
@@ -30,15 +69,20 @@ fn main() {
         .expect("Should have been able to read the file");
     let test_config: TestConfig = toml::from_str(&toml_str).unwrap();
 
-    let mut cmd = Command::new(test_config.test_program);
-    cmd.args(test_config.test_arguments.unwrap_or(vec![]));
+    let test_case = test_config.to_test_case().unwrap();
+    run_test_case(test_case)
+}
+
+fn run_test_case(test_case: TestCase) {
+    let mut cmd = Command::new(test_case.program);
+    cmd.args(test_case.arguments);
     cmd.stdin(Stdio::piped());
     cmd.stdout(Stdio::piped());
     cmd.stderr(Stdio::piped());
 
     let mut child = cmd.spawn().unwrap();
 
-    if let Some(stdin_string) = test_config.test_stdin {
+    if let Some(stdin_string) = test_case.stdin {
         let mut stdin = child.stdin.take().unwrap();
         stdin.write_all(stdin_string.as_bytes()).unwrap();
     }
@@ -58,10 +102,15 @@ fn main() {
     let exit_status = child.wait().unwrap();
     let exit_code = exit_status.code().unwrap();
 
-    let is_success =
-        test_config.expected_stdout.map(|x| { x == stdout_string }).unwrap_or(true) &&
-        test_config.expected_stderr.map(|x| { x == stderr_string }).unwrap_or(true) &&
-        test_config.expected_exit_code.map(|x| { x == exit_code }).unwrap_or(true);
+    let is_success = test_case.assertions.into_iter().all(|assertion| { check_assertion(&stdout_string, &stderr_string, &exit_code, &assertion) });
 
     println!("{} - {} '{}' '{}'", if is_success { "ok" } else { "not ok" }, exit_code, stdout_string, stderr_string);
+}
+
+fn check_assertion(stdout: &String, stderr: &String, exit_code: &i32, assertion: &TestAssertion) -> bool {
+    match assertion {
+        TestAssertion::AssertStdout(expected_stdout) => stdout == expected_stdout,
+        TestAssertion::AssertStderr(expected_stderr) => stderr == expected_stderr,
+        TestAssertion::AssertExitCode(expected_exit_code) => exit_code == expected_exit_code,
+    }
 }
