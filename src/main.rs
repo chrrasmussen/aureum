f8a9eb672669fde83daf33faf6e89504b59a591d
Christian Rasmussen <christian.rasmussen@me.com>
2023-01-03 22:28:27 +0100

    Move test_case to its own module

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+mod test_case;
+
 use std::fs;
-use std::io::{self, Write, Read};
-use std::process::{Command, Stdio};
 use std::str;
 use serde::Deserialize;
 use clap::Parser;
+use test_case::{TestCase, TestAssertion, TestOutput};
+
 
 /// Golden test runner
 #[derive(Parser, Debug)]
@@ -48,32 +50,6 @@ enum TestConfigError {
     ExpectationRequired,
 }
 
-struct TestCase {
-    program: String,
-    arguments: Vec<String>,
-    stdin: Option<String>,
-    assertions: Vec<TestAssertion>,
-}
-
-enum TestAssertion {
-    AssertStdout(String),
-    AssertStderr(String),
-    AssertExitCode(i32),
-}
-
-struct TestOutput {
-    stdout: String,
-    stderr: String,
-    exit_code: i32,
-}
-
-#[derive(Debug)]
-enum TestError {
-    FailedToDecodeUtf8,
-    MissingExitCode,
-    IOError(io::Error),
-}
-
 
 fn main() {
     let args = Args::parse();
@@ -83,44 +59,11 @@ fn main() {
     let test_config: TestConfig = toml::from_str(&toml_str).unwrap();
 
     let test_case = test_config.to_test_case().unwrap();
-    let test_output = run_test_case(&test_case).unwrap();
+    let test_output = test_case::run(&test_case).unwrap();
 
     let is_success = test_case.assertions.into_iter().all(|assertion| check_assertion(&test_output, &assertion));
 
     println!("{} - {} '{}' '{}'", if is_success { "ok" } else { "not ok" }, test_output.exit_code, test_output.stdout, test_output.stderr);
-}
-
-fn run_test_case(test_case: &TestCase) -> Result<TestOutput, TestError> {
-    let mut cmd = Command::new(&test_case.program);
-    cmd.args(&test_case.arguments);
-    cmd.stdin(Stdio::piped());
-    cmd.stdout(Stdio::piped());
-    cmd.stderr(Stdio::piped());
-
-    let mut child = cmd.spawn().map_err(TestError::IOError)?;
-
-    if let Some(stdin_string) = &test_case.stdin {
-        let mut stdin = child.stdin.take().expect("Stdin should be configured to pipe");
-        stdin.write_all(stdin_string.as_bytes()).map_err(TestError::IOError)?;
-    }
-
-    let stdout = read_pipe_to_string(&mut child.stdout.take().expect("Stdout should be configured to pipe"))?;
-    let stderr = read_pipe_to_string(&mut child.stderr.take().expect("Stderr should be configured to pipe"))?;
-
-    let exit_status = child.wait().map_err(TestError::IOError)?;
-    let exit_code = exit_status.code().map_or(Err(TestError::MissingExitCode), Ok)?;
-
-    Ok(TestOutput {
-        stdout,
-        stderr,
-        exit_code,
-    })
-}
-
-fn read_pipe_to_string<T>(pipe: &mut T) -> Result<String, TestError> where T: Read {
-    let mut buf: Vec<u8> = vec![];
-    pipe.read_to_end(&mut buf).map_err(TestError::IOError)?;
-    String::from_utf8(buf).map_or(Err(TestError::FailedToDecodeUtf8), Ok)
 }
 
 fn check_assertion(output: &TestOutput, assertion: &TestAssertion) -> bool {
---
+++ src/test_case.rs
@@ -0,0 +1,61 @@
+use std::io::{self, Write, Read};
+use std::process::{Command, Stdio};
+
+pub struct TestCase {
+    pub program: String,
+    pub arguments: Vec<String>,
+    pub stdin: Option<String>,
+    pub assertions: Vec<TestAssertion>,
+}
+
+pub enum TestAssertion {
+    AssertStdout(String),
+    AssertStderr(String),
+    AssertExitCode(i32),
+}
+
+pub struct TestOutput {
+    pub stdout: String,
+    pub stderr: String,
+    pub exit_code: i32,
+}
+
+#[derive(Debug)]
+pub enum TestError {
+    FailedToDecodeUtf8,
+    MissingExitCode,
+    IOError(io::Error),
+}
+
+pub fn run(test_case: &TestCase) -> Result<TestOutput, TestError> {
+    let mut cmd = Command::new(&test_case.program);
+    cmd.args(&test_case.arguments);
+    cmd.stdin(Stdio::piped());
+    cmd.stdout(Stdio::piped());
+    cmd.stderr(Stdio::piped());
+
+    let mut child = cmd.spawn().map_err(TestError::IOError)?;
+
+    if let Some(stdin_string) = &test_case.stdin {
+        let mut stdin = child.stdin.take().expect("Stdin should be configured to pipe");
+        stdin.write_all(stdin_string.as_bytes()).map_err(TestError::IOError)?;
+    }
+
+    let stdout = read_pipe_to_string(&mut child.stdout.take().expect("Stdout should be configured to pipe"))?;
+    let stderr = read_pipe_to_string(&mut child.stderr.take().expect("Stderr should be configured to pipe"))?;
+
+    let exit_status = child.wait().map_err(TestError::IOError)?;
+    let exit_code = exit_status.code().map_or(Err(TestError::MissingExitCode), Ok)?;
+
+    Ok(TestOutput {
+        stdout,
+        stderr,
+        exit_code,
+    })
+}
+
+fn read_pipe_to_string<T>(pipe: &mut T) -> Result<String, TestError> where T: Read {
+    let mut buf: Vec<u8> = vec![];
+    pipe.read_to_end(&mut buf).map_err(TestError::IOError)?;
+    String::from_utf8(buf).map_or(Err(TestError::FailedToDecodeUtf8), Ok)
+}
