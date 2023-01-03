c61d9168c4f5ef89978482df1590ef630cc71a94
Christian Rasmussen <christian.rasmussen@me.com>
2023-01-04 00:56:26 +0100

    Allow stdin/stdout/stderr/exitcode be specified in external file

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ mod test_case;
 use clap::Parser;
 use serde::Deserialize;
 use std::fs;
-use std::str;
+use std::io;
+use std::str::FromStr;
 use test_case::{TestAssertion, TestCase, TestOutput};
 
 /// Golden test runner
@@ -15,25 +16,39 @@ struct Args {
 
 #[derive(Deserialize)]
 struct TestConfig {
-    test_program: String,
-    test_arguments: Option<Vec<String>>,
-    test_stdin: Option<String>,
-    expected_stdout: Option<String>,
-    expected_stderr: Option<String>,
-    expected_exit_code: Option<i32>,
+    test_program: ConfigValue<String>,
+    test_arguments: Option<Vec<ConfigValue<String>>>,
+    test_stdin: Option<ConfigValue<String>>,
+    expected_stdout: Option<ConfigValue<String>>,
+    expected_stderr: Option<ConfigValue<String>>,
+    expected_exit_code: Option<ConfigValue<i32>>,
 }
 
 impl TestConfig {
     fn to_test_case(self) -> Result<TestCase, TestConfigError> {
+        let program = self.test_program.read()?;
+
+        let mut arguments = vec![];
+        for arg in self.test_arguments.unwrap_or(vec![]) {
+            arguments.push(arg.read()?)
+        }
+
+        let stdin: Option<String>;
+        if let Some(test_stdin) = self.test_stdin {
+            stdin = Some(test_stdin.read()?)
+        } else {
+            stdin = None
+        }
+
         let mut assertions = vec![];
         if let Some(stdout) = self.expected_stdout {
-            assertions.push(TestAssertion::AssertStdout(stdout))
+            assertions.push(TestAssertion::AssertStdout(stdout.read()?))
         }
         if let Some(stderr) = self.expected_stderr {
-            assertions.push(TestAssertion::AssertStderr(stderr))
+            assertions.push(TestAssertion::AssertStderr(stderr.read()?))
         }
         if let Some(exit_code) = self.expected_exit_code {
-            assertions.push(TestAssertion::AssertExitCode(exit_code))
+            assertions.push(TestAssertion::AssertExitCode(exit_code.read()?))
         }
 
         if assertions.len() == 0 {
@@ -41,9 +56,9 @@ impl TestConfig {
         }
 
         Ok(TestCase {
-            program: self.test_program,
-            arguments: self.test_arguments.unwrap_or(vec![]),
-            stdin: self.test_stdin,
+            program,
+            arguments,
+            stdin,
             assertions,
         })
     }
@@ -51,7 +66,36 @@ impl TestConfig {
 
 #[derive(Debug)]
 enum TestConfigError {
+    FailedToParseString(String),
     ExpectationRequired,
+    IOError(io::Error),
+}
+
+#[derive(Deserialize)]
+#[serde(untagged)]
+enum ConfigValue<T> {
+    Literal(T),
+    WrappedLiteral { value: T },
+    ReadFromFile { file: String },
+}
+
+impl<T> ConfigValue<T>
+where
+    T: FromStr,
+{
+    fn read(self) -> Result<T, TestConfigError> {
+        match self {
+            Self::Literal(value) => Ok(value),
+            Self::WrappedLiteral { value } => Ok(value),
+            Self::ReadFromFile { file } => {
+                let str = fs::read_to_string(file).map_err(TestConfigError::IOError)?;
+                let value = str
+                    .parse()
+                    .map_err(|_err| TestConfigError::FailedToParseString(str))?;
+                Ok(value)
+            }
+        }
+    }
 }
 
 fn main() {
