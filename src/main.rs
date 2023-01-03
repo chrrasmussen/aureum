2aa1fb98d29f352939afc3c6fd24cd1fcc67bd9a
Christian Rasmussen <christian.rasmussen@me.com>
2023-01-03 22:18:28 +0100

    Refactor error handling

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::fs;
-use std::io::{Write, Read};
+use std::io::{self, Write, Read};
 use std::process::{Command, Stdio};
 use std::str;
 use serde::Deserialize;
@@ -61,6 +61,19 @@ enum TestAssertion {
     AssertExitCode(i32),
 }
 
+struct TestOutput {
+    stdout: String,
+    stderr: String,
+    exit_code: i32,
+}
+
+#[derive(Debug)]
+enum TestError {
+    FailedToDecodeUtf8,
+    MissingExitCode,
+    IOError(io::Error),
+}
+
 
 fn main() {
     let args = Args::parse();
@@ -70,47 +83,50 @@ fn main() {
     let test_config: TestConfig = toml::from_str(&toml_str).unwrap();
 
     let test_case = test_config.to_test_case().unwrap();
-    run_test_case(test_case)
+    let test_output = run_test_case(&test_case).unwrap();
+
+    let is_success = test_case.assertions.into_iter().all(|assertion| check_assertion(&test_output, &assertion));
+
+    println!("{} - {} '{}' '{}'", if is_success { "ok" } else { "not ok" }, test_output.exit_code, test_output.stdout, test_output.stderr);
 }
 
-fn run_test_case(test_case: TestCase) {
-    let mut cmd = Command::new(test_case.program);
-    cmd.args(test_case.arguments);
+fn run_test_case(test_case: &TestCase) -> Result<TestOutput, TestError> {
+    let mut cmd = Command::new(&test_case.program);
+    cmd.args(&test_case.arguments);
     cmd.stdin(Stdio::piped());
     cmd.stdout(Stdio::piped());
     cmd.stderr(Stdio::piped());
 
-    let mut child = cmd.spawn().unwrap();
+    let mut child = cmd.spawn().map_err(TestError::IOError)?;
 
-    if let Some(stdin_string) = test_case.stdin {
-        let mut stdin = child.stdin.take().unwrap();
-        stdin.write_all(stdin_string.as_bytes()).unwrap();
+    if let Some(stdin_string) = &test_case.stdin {
+        let mut stdin = child.stdin.take().expect("Stdin should be configured to pipe");
+        stdin.write_all(stdin_string.as_bytes()).map_err(TestError::IOError)?;
     }
 
-    let mut stdout = child.stdout.take().unwrap();
-    let mut buf: Vec<u8> = vec![];
-    stdout.read_to_end(&mut buf).unwrap();
+    let stdout = read_pipe_to_string(&mut child.stdout.take().expect("Stdout should be configured to pipe"))?;
+    let stderr = read_pipe_to_string(&mut child.stderr.take().expect("Stderr should be configured to pipe"))?;
 
-    let stdout_string = String::from_utf8(buf).unwrap();
+    let exit_status = child.wait().map_err(TestError::IOError)?;
+    let exit_code = exit_status.code().map_or(Err(TestError::MissingExitCode), Ok)?;
 
-    let mut stderr = child.stderr.take().unwrap();
-    let mut buf: Vec<u8> = vec![];
-    stderr.read_to_end(&mut buf).unwrap();
-
-    let stderr_string = String::from_utf8(buf).unwrap();
-
-    let exit_status = child.wait().unwrap();
-    let exit_code = exit_status.code().unwrap();
-
-    let is_success = test_case.assertions.into_iter().all(|assertion| { check_assertion(&stdout_string, &stderr_string, &exit_code, &assertion) });
-
-    println!("{} - {} '{}' '{}'", if is_success { "ok" } else { "not ok" }, exit_code, stdout_string, stderr_string);
+    Ok(TestOutput {
+        stdout,
+        stderr,
+        exit_code,
+    })
 }
 
-fn check_assertion(stdout: &String, stderr: &String, exit_code: &i32, assertion: &TestAssertion) -> bool {
+fn read_pipe_to_string<T>(pipe: &mut T) -> Result<String, TestError> where T: Read {
+    let mut buf: Vec<u8> = vec![];
+    pipe.read_to_end(&mut buf).map_err(TestError::IOError)?;
+    String::from_utf8(buf).map_or(Err(TestError::FailedToDecodeUtf8), Ok)
+}
+
+fn check_assertion(output: &TestOutput, assertion: &TestAssertion) -> bool {
     match assertion {
-        TestAssertion::AssertStdout(expected_stdout) => stdout == expected_stdout,
-        TestAssertion::AssertStderr(expected_stderr) => stderr == expected_stderr,
-        TestAssertion::AssertExitCode(expected_exit_code) => exit_code == expected_exit_code,
+        TestAssertion::AssertStdout(expected_stdout) => &output.stdout == expected_stdout,
+        TestAssertion::AssertStderr(expected_stderr) => &output.stderr == expected_stderr,
+        TestAssertion::AssertExitCode(expected_exit_code) => &output.exit_code == expected_exit_code,
     }
 }
