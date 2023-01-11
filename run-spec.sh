4ea2d66e25116e4a12a89cc3b2068e68051a7044
Christian Rasmussen <christian.rasmussen@me.com>
2023-01-12 00:43:00 +0100

    Test running the aureum commmand itself

--- run-spec.sh
+++ run-spec.sh
@@ -7,3 +7,5 @@ cargo run -- spec/basic02/test.toml
 cargo run -- spec/basic03/test.toml
 cargo run -- spec/basic04/test.toml
 AUREUM_MY_CUSTOM_ENV_VAR="Hello world" cargo run -- spec/basic05/test.toml
+
+AUREUM_EXEC="$PWD/target/debug/aureum" cargo run -- spec/aureum01/test.toml
