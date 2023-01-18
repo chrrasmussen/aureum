6310e53d5f46a410c1fe358b2c48528e14fac444
Christian Rasmussen <christian.rasmussen@me.com>
2023-01-18 01:03:38 +0100

    Allow additional args to run-spec.sh

--- run-spec.sh
+++ run-spec.sh
@@ -5,4 +5,6 @@ set -e
 export AUREUM_EXEC="$PWD/target/debug/aureum"
 export AUREUM_TEST_HELLO_WORLD="Hello world" # Required by `basic05`
 
-cargo run -- spec.au.toml
+ARGS=${*:-spec.au.toml}
+
+cargo run -- $ARGS
