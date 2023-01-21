d060c0752e301bc545bc1cc7ff090b807120db68
Christian Rasmussen <christian.rasmussen@me.com>
2023-01-21 02:38:19 +0100

    Move run_test_cases to test_runner module

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 mod tap_format;
 mod test_case;
 mod test_config;
+mod test_runner;
 
 use clap::Parser;
 use glob::glob;
-use rayon::prelude::*;
 use std::collections::BTreeSet;
 use std::process::exit;
 use std::{fs, io, path::PathBuf};
-use test_case::{RunError, TestCase, TestResult};
+use test_case::TestCase;
 
 const EXIT_CODE_ON_FAILURE: i32 = 1;
 
@@ -18,10 +18,6 @@ struct Args {
     /// Paths to test configs
     #[structopt(required = true)]
     paths: Vec<String>,
-}
-
-struct ReportConfig {
-    number_of_tests: usize,
 }
 
 fn main() {
@@ -42,70 +38,18 @@ fn main() {
         }
     }
 
-    let report_config = ReportConfig {
+    let report_config = test_runner::ReportConfig {
         number_of_tests: test_cases.len(),
     };
 
-    report_start(&report_config);
-    let all_successful = run_test_cases(&report_config, &test_cases, false);
+    test_runner::report_start(&report_config);
+    let all_successful = test_runner::run_test_cases(&report_config, &test_cases, false);
 
     // TODO: Print failing configs
 
     if all_successful == false {
         exit(EXIT_CODE_ON_FAILURE)
     }
-}
-
-fn run_test_cases(
-    report_config: &ReportConfig,
-    test_cases: &[TestCase],
-    run_in_parallel: bool,
-) -> bool {
-    let run = |(i, test_case)| -> bool {
-        report_test_result(
-            report_config,
-            i,
-            test_case,
-            test_case::run(test_case.clone()),
-        )
-    };
-
-    if run_in_parallel {
-        test_cases
-            .par_iter()
-            .enumerate()
-            .map(run)
-            .reduce(|| true, |x, y| x && y)
-    } else {
-        test_cases
-            .iter()
-            .enumerate()
-            .map(run)
-            .fold(true, |x, y| x && y)
-    }
-}
-
-fn report_start(report_config: &ReportConfig) {
-    tap_format::print_version();
-    tap_format::print_plan(1, report_config.number_of_tests);
-}
-
-fn report_test_result(
-    report_config: &ReportConfig,
-    index: usize,
-    test_case: &TestCase,
-    result: Result<TestResult, RunError>,
-) -> bool {
-    let test_number_indent_level = report_config.number_of_tests.to_string().len();
-    print_test_case_result(index + 1, &test_case, &result, test_number_indent_level);
-
-    if let Ok(result) = &result {
-        if test_case::expectations_fulfilled(result) {
-            return true;
-        }
-    }
-
-    false
 }
 
 enum TestFileError {
@@ -121,36 +65,6 @@ fn test_cases_from_file(test_file: &PathBuf) -> Result<Vec<TestCase>, TestFileEr
     test_config
         .to_test_cases(test_file)
         .map_err(TestFileError::FailedToReadTestCases)
-}
-
-fn print_test_case_result(
-    test_number: usize,
-    case: &TestCase,
-    result: &Result<TestResult, RunError>,
-    indent_level: usize,
-) {
-    let is_success: bool;
-    match result {
-        Ok(test_result) => is_success = test_case::expectations_fulfilled(test_result),
-        Err(_) => is_success = false,
-    }
-
-    let message: String;
-    if let Some(description) = &case.description {
-        message = format!(
-            "{} # {}",
-            case.source_file.display().to_string(),
-            description,
-        );
-    } else {
-        message = format!("{}", case.source_file.display().to_string(),);
-    }
-
-    if is_success {
-        tap_format::print_ok(test_number, &message, indent_level)
-    } else {
-        tap_format::print_not_ok(test_number, &message, "", indent_level)
-    }
 }
 
 fn locate_test_files(path: &str, test_files: &mut BTreeSet<PathBuf>) {
---
+++ src/test_runner.rs
@@ -0,0 +1,89 @@
+use crate::tap_format;
+use crate::test_case::{self, RunError, TestCase, TestResult};
+use rayon::prelude::*;
+
+pub struct ReportConfig {
+    pub number_of_tests: usize,
+}
+
+pub fn report_start(report_config: &ReportConfig) {
+    tap_format::print_version();
+    tap_format::print_plan(1, report_config.number_of_tests);
+}
+
+pub fn run_test_cases(
+    report_config: &ReportConfig,
+    test_cases: &[TestCase],
+    run_in_parallel: bool,
+) -> bool {
+    let run = |(i, test_case)| -> bool {
+        report_test_result(
+            report_config,
+            i,
+            test_case,
+            test_case::run(test_case.clone()),
+        )
+    };
+
+    if run_in_parallel {
+        test_cases
+            .par_iter()
+            .enumerate()
+            .map(run)
+            .reduce(|| true, |x, y| x && y)
+    } else {
+        test_cases
+            .iter()
+            .enumerate()
+            .map(run)
+            .fold(true, |x, y| x && y)
+    }
+}
+
+fn report_test_result(
+    report_config: &ReportConfig,
+    index: usize,
+    test_case: &TestCase,
+    result: Result<TestResult, RunError>,
+) -> bool {
+    let test_number_indent_level = report_config.number_of_tests.to_string().len();
+    print_test_case_result(index + 1, &test_case, &result, test_number_indent_level);
+
+    if let Ok(result) = &result {
+        if test_case::expectations_fulfilled(result) {
+            return true;
+        }
+    }
+
+    false
+}
+
+fn print_test_case_result(
+    test_number: usize,
+    case: &TestCase,
+    result: &Result<TestResult, RunError>,
+    indent_level: usize,
+) {
+    let is_success: bool;
+    match result {
+        Ok(test_result) => is_success = test_case::expectations_fulfilled(test_result),
+        Err(_) => is_success = false,
+    }
+
+    let message: String;
+    if let Some(description) = &case.description {
+        message = format!(
+            "{} # {}",
+            case.source_file.display().to_string(),
+            description,
+        );
+    } else {
+        message = format!("{}", case.source_file.display().to_string(),);
+    }
+
+    if is_success {
+        tap_format::print_ok(test_number, &message, indent_level)
+    } else {
+        tap_format::print_not_ok(test_number, &message, "", indent_level)
+    }
+}
