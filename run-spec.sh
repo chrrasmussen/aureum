785f5dfbb12ca084e6bf60dad1c2bbcbb59a446e
Christian Rasmussen <christian.rasmussen@me.com>
2023-01-24 04:45:22 +0100

    Rename AUREUM_EXEC to show that it is related to tests

--- run-spec.sh
+++ run-spec.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-export AUREUM_EXEC="$PWD/target/debug/aureum"
+export AUREUM_TEST_EXEC="$PWD/target/debug/aureum"
 export AUREUM_TEST_HELLO_WORLD="Hello world" # Required by `basic05`
 
 ARGS=${*:-spec.au.toml}
