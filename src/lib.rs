10afbc97b764bec373a7564727a50cc67594f3a7
Christian Rasmussen <christian.rasmussen@me.com>
2023-02-19 16:06:55 +0100

    Restructure modules in library

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,15 @@
 mod ascii_tree;
-pub mod formats;
+pub mod formats {
+    pub mod tap;
+    pub mod tree;
+}
 pub mod test_case;
 pub mod test_id;
 pub mod test_id_container;
 pub mod test_result;
 pub mod test_runner;
 pub mod toml_config;
-pub mod utils;
+pub mod utils {
+    pub mod file;
+    pub mod string;
+}
